75dd28b8a8ba0646a110ded4ed26896f0a549c3c
bokisha <bojants91@gmail.com>
2016-04-15 00:37:22 +0200

    dodat naslov, promene

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 clear
 
-pdflatex MilenkovicNestorovicSojcic.tex
-bibtex MilenkovicNestorovicSojcic.tex
-pdflatex MilenkovicNestorovicSojcic.tex
-pdflatex MilenkovicNestorovicSojcic.tex
+pdflatex 2_MilenkovicNestorovicSojcic.tex
+bibtex 2_MilenkovicNestorovicSojcic.tex
+pdflatex 2_MilenkovicNestorovicSojcic.tex
+pdflatex 2_MilenkovicNestorovicSojcic.tex
 
 echo "PDF je izgenerisan"
 echo
